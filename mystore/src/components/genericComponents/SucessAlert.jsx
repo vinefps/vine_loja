3d15fd00561022399185e5fdc7c70bfb205d2419
Vinícius de Oliveira <112340022+vinefps@users.noreply.github.com>
2025-03-22 16:16:58 -0300

    Update SucessAlert.jsx

--- mystore/src/components/genericComponents/SucessAlert.jsx
+++ mystore/src/components/genericComponents/SucessAlert.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-
-export const SucessAlert = ({title}) => {
+// SucessAlert.jsx
+export function SucessAlert({ title }) {
   return (
-    <div className="space-y-2 p-4">
+    <div className="space-y-2 p-2">
       <div
         role="alert"
-        className="bg-green-100 dark:bg-green-900 border-l-4 border-green-500 dark:border-green-700 text-green-900 dark:text-green-100 p-2 rounded-lg flex items-center transition duration-300 ease-in-out hover:bg-green-200 dark:hover:bg-green-800 transform hover:scale-105"
+        className="bg-green-100 dark:bg-green-900/30 border-l-4 border-green-500 dark:border-green-700 text-green-700 dark:text-green-300 p-4 rounded-lg flex items-center animate-fadeIn shadow-sm"
       >
         <svg
           stroke="currentColor"
           viewBox="0 0 24 24"
           fill="none"
-          className="h-5 w-5 flex-shrink-0 mr-2 text-green-600"
+          className="h-5 w-5 flex-shrink-0 mr-3 text-green-500"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            d="M5 13l4 4L19 7"
             strokeWidth="2"
             strokeLinejoin="round"
             strokeLinecap="round"
           ></path>
         </svg>
-        <p className="text-xs font-semibold">{title}</p>
+        <p className="text-sm font-medium">{title}</p>
       </div>
     </div>
   );
-};
-
-
+}
